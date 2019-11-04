dc995b3d24d4d3d92a3367f10bd4bfab607c8bd3
bhaskar <eyutam0@gmail.com>
2019-11-04 15:23:48 -0700

    added

---
+++ mygitproject/src/mygitpackage/MainClass.java
@@ -0,0 +1,10 @@
+package mygitpackage;
+
+public class MainClass {
+	public void main(String [] args) {
+Student obj=new Student();
+obj.setName("Eyasu");
+obj.setId(1234);
+System.out.println("name :"+obj.getName()+ " id :"+obj.getId());
+	}
+}
---
+++ mygitproject/src/mygitpackage/Student.java
@@ -0,0 +1,25 @@
+package mygitpackage;
+
+public class Student {
+	int id;
+	String name;
+	public void setName(String name) {
+		this.name=name;
+		
+	}
+	public void setId(int id) {
+		this.id=id;
+	
+	}
+	public String getName() {
+		return this.name;
+		
+	}
+   public int getId() {
+	   return this.id;
+   }
+   public void display() 
+   {
+	   System.out.println("name "+this.name +"id "+this.id);
+   }
+}
